59c11abff0011f64fc0d2d970da4ff6b17860291
MD Alimuzzaman Haris <73691596+Az-Haris@users.noreply.github.com>
2022-02-02 21:26:34 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    int marks[10]={2342,245,54564,6575,2312,565,322,456,6756,676567};
+    int a;
+
+    for(a=0; a<10; a++)
+    {
+        printf("%d\t\n", marks[a]);
+    }
+
+
+    getch();
+    return 0;
+}
